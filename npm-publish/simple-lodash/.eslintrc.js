1a1c1d3c00b30c11bc93ffcdac88498bf75f7e13
looper.m.zhuo <2390903545@qq.com>
2021-07-13 23:33:22 +0800

    修改eslint规则

--- npm-publish/simple-lodash/.eslintrc.js
+++ npm-publish/simple-lodash/.eslintrc.js
@@ -7,8 +7,11 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
+  extends: "eslint:recommended",
   rules: {
+    "no-undef": 0,
     "object-curly-newline": "off",
     "no-plusplus": "off",
+    semi: 1,
   },
 };
--- npm-publish/simple-lodash/types/index.d.ts
+++ npm-publish/simple-lodash/types/index.d.ts
@@ -3,18 +3,18 @@
  * @param data any
  * @returns string
  */
-export function accurateType(data: any) {}
+export function accurateType(data: any): string {}
 
 /**
  * 将伪数组转真数组
  * @param data  pseudo-array
  * @returns Array
  */
-export function pseudoArr2Array(data: any) {}
+export function pseudoArr2Array(data: any): Array {}
 
 /**
  * 深拷贝的实现
  * @param data any
  * @returns any
  */
-export function deepClone(data: any) {}
+export function deepClone(data: any): any {}
