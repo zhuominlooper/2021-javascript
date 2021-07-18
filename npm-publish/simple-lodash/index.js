275b2f465eab018a950a65cb1ef74ec6d15b7eb1
looper.m.zhuo <2390903545@qq.com>
2021-07-18 22:42:31 +0800

    解决打包冲突问题

--- npm-publish/simple-lodash/index.js
+++ npm-publish/simple-lodash/index.js
@@ -2,3 +2,4 @@ const dotenv = require("dotenv");
 dotenv.config();
 module.exports = require("./lib/app.js");
 require("@babel/polyfill"); //引入该bable插件主要可以转换高级的实例上api，includes
+console.log(process.env.NODE_ENV);
