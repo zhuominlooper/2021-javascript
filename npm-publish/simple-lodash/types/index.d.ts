39850be6cd53b2801573005c81b96db078d8c0c2
looper.m.zhuo <2390903545@qq.com>
2021-07-31 15:26:48 +0800

    测试CI

--- npm-publish/simple-lodash/types/index.d.ts
+++ npm-publish/simple-lodash/types/index.d.ts
@@ -18,60 +18,60 @@ export function pseudoArr2Array(data: any): Array {}
  * @param data any
  * @returns any
  */
-export function deepClone(data: any) {}
+export function deepClone(data: any): any {}
 
 /**
  * 多维数组转一维数组
  * @param data Array
  * @returns any
  */
-export function convertDimensional(data: Array) {}
+export function convertDimensional(data: Array): Array {}
 
 /**
  * 冒泡排序
  * @param data Array
  * @returns Object
  */
-export function counterfeitSort(data: Array) {}
+export function counterfeitSort(data: Array): any {}
 
 /**
  * 去掉空格
  * @param data String
  * @returns String
  */
-export function trim(data: String) {}
+export function trim(data: String): String {}
 
 /**
  * 数组中的总和
  * @param data Array
  * @returns Number
  */
-export function arrNumSum(data: Array) {}
+export function arrNumSum(data: Array): Number {}
 
 /**
  * 数组中的最大值
  * @param data Array
  * @returns Number
  */
-export function arrNumMax(data: Array) {}
+export function arrNumMax(data: Array): Number {}
 
 /**
  * 数组中的最小值
  * @param data Array
  * @returns Number
  */
-export function arrNumMin(data: Array) {}
+export function arrNumMin(data: Array): Number {}
 
 /**
  * 返回数组中的非假数值
  * @param data Array
  * @returns Array
  */
-export function compact(data: Array) {}
+export function compact(data: Array): Number {}
 
 /**
  * 返回img实例
  * @param data Array
  * @returns Array
  */
-export function getImgInstance(url: String, data?: imgOptions) {}
+export function getImgInstance(url: String, data?: imgOptions): Array {}
